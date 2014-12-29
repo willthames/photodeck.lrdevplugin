4e6900c164a7a8aed9e97b756f8ae273307c1b20
Will Thames <will@thames.id.au>
2014-12-29 14:40:48 +1000

    Fix HTTP error handling callback
    onerror uses the status code, not the full status message

--- PhotoDeckAPI.lua
+++ PhotoDeckAPI.lua
@@ -98,8 +98,9 @@ local function handle_errors(response, resp_headers, onerror)
   local status = PhotoDeckUtils.filter(resp_headers, function(v) return isTable(v) and v.field == 'Status' end)[1]
 
   if not status or status.value > "400" then
-    if onerror and onerror[status] then
-      return onerror[status]()
+    local statuscode = string.sub(status.value, 1, 3)
+    if onerror and onerror[statuscode] then
+      return onerror[statuscode]()
     end
     logger:error("Bad response: " .. response)
     logger:error(PhotoDeckUtils.printLrTable(resp_headers))
